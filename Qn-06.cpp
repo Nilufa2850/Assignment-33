369f22fb86d8ac52417cc51669ac4a376d6dc920
Nilufa Yasmin <116085061+Nilufa2850@users.noreply.github.com>
2023-08-15 22:19:06 +0530

    Create Qn-06.cpp

---
+++ Qn-06.cpp
@@ -0,0 +1,8 @@
+/*6. Extend above to display the area of circles. For a circle,only one value is needed i.e.
+radius but in get_data() function 2 values are passed.*/
+
+#include<iostream>
+using namespace std ;
+
+// Same as qn-02 of Asiignment 33
+
